918dd4927097a2b795021c0997e96621b1ff0d6a
Will Johnson <williamterryjohnsonjr@gmail.com>
2016-06-27 21:07:16 -0400

    Changed form from class=form to id=mainForm in index.html, and adjusted main.scss and main.js accordingly. Updated main.js to inject <select> and <textarea> tags into index.html.

--- src/js/main.js
+++ src/js/main.js
@@ -7,39 +7,17 @@ var formData = $.ajax({
 });
 
 formData.then(function(data){
-	// var optionString="";
-	// var optionMaker = function(item){
-	// 			return `<option value="${item.value}">${item.label}</option>`;
-	// };
 	data.forEach(function(object){
-		if (object.type === "text" || object.type === "email" || object.type === "tel")
-		{
-			$(".form").append(`<i class="fa ${object.icon}" aria-hidden="true"></i><input id="${object.id}" type="${object.type}" placeholder="${object.label}">`)
+		if (object.type === "text" || object.type === "email" || object.type === "tel"){
+			$("#mainForm").append(`<i class="fa ${object.icon}" aria-hidden="true"></i><input id="${object.id}" type="${object.type}" placeholder="${object.label}">`)
 		} else if (object.type ==="select"){
-			$(".form").append(`<select id="${object.id}">${object.options.forEach(function(item){
+			$("#mainForm").append(`<select id="${object.id}">${object.options.forEach(function(item){
 				return `<option value="${item.value}">${item.label}</option>`;})}</select>`);
+		} else if (object.type === "textarea") {
+			$("#mainForm").append(`<i class="fa ${object.icon}" aria-hidden="true"></i> <textarea id="commentsBox" rows="5" cols="40" placeholder="Your comment"></textarea>`)
 		}
 	});
 			
-
-
-// object.options[1]
-
-// formData.responseJSON[4]options
-
-// var languages = [];
-// 	formData.responseJSON[4]options.forEach(function(object){
-// 		languages.push(object.label);
-// 	});
-
-// 	languages.forEach(function(item){
-// 		$("#languages").append("<option value=languages[count]>")
-
-// 	document.getElementById("user-language") = `<input id="${formData.responseJSON[4].id}" type="${formData.responseJSON[4].type}" placeholder="${formData.responseJSON[4].label}" list="languages">`;
-// 	// var datalistLangs = $("#languages").html()
-// 	// formData.responseJSON[4].options.forEach(function(object){
-
-
-
-// 	// }));
+	// formData.responseJSON[4].options.forEach(function(object){
+	// }));
 });
