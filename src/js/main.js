a696ef94abc3a70aa52ad79550864f56c957baf3
Will Johnson <williamterryjohnsonjr@gmail.com>
2016-06-28 14:37:13 -0400

    Updated main.js to reflect Brit's approach in homework review. Got the Select menu working
    n
    !

--- src/js/main.js
+++ src/js/main.js
@@ -6,18 +6,35 @@ var formData = $.ajax({
 	error: console.log("We have a problem.")
 });
 
+var textInput = function (object) {
+$("#mainForm").append(`
+	<i class="fa ${object.icon}" aria-hidden="true"></i>
+	<input id="${object.id}" type="${object.type}" placeholder="${object.label}">`);
+};
+
+var textAreaTag = function (object) {
+	$("#mainForm").append(`
+		<i class="fa ${object.icon}" aria-hidden="true"></i>
+		<textarea id="commentsBox" placeholder="Your comment"></textarea>`);
+};
+
+var optionTag = function(object) {
+	return `<option value="${object.value}">${object.label}</option>`;
+};
+
+var selectTag = function (object){
+	$("#mainForm").append(`<select id="${object.id}"><option value="">Select Language...</option>${object.options.map(optionTag).join('')}</select>`)
+};
+
 formData.then(function(data){
 	data.forEach(function(object){
-		if (object.type === "text" || object.type === "email" || object.type === "tel"){
-			$("#mainForm").append(`<i class="fa ${object.icon}" aria-hidden="true"></i><input id="${object.id}" type="${object.type}" placeholder="${object.label}">`)
-		} else if (object.type ==="select"){
-			$("#mainForm").append(`<select id="${object.id}">${object.options.forEach(function(item){
-				return `<option value="${item.value}">${item.label}</option>`;})}</select>`);
-		} else if (object.type === "textarea") {
-			$("#mainForm").append(`<i class="fa ${object.icon}" aria-hidden="true"></i> <textarea id="commentsBox" rows="5" cols="40" placeholder="Your comment"></textarea>`)
+		if (object.type ==="select"){
+			selectTag(object);
+		} else 
+		if (object.type === "textarea") {
+			textAreaTag(object);
+		} else {
+			textInput(object);
 		}
 	});
-			
-	// formData.responseJSON[4].options.forEach(function(object){
-	// }));
 });
